078df051688d172a14eb2ee576fc309f1ec1fec2
Andrew67 <andres@andrew67.com>
2024-01-25 18:50:44 -0500

    Create and add dark theme. Fix missing migrated Reload/Retry buttons.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -24,7 +24,23 @@ export default {
           error: colors.red["800"],
         },
       },
+      {
+        "ddrfinder-dark": {
+          primary: colors.blue["200"],
+          secondary: colors.fuchsia["200"],
+          accent: colors.lime["200"],
+          neutral: colors.slate["100"],
+          "base-100": colors.slate["900"],
+          "base-200": colors.slate["800"],
+          "base-300": colors.slate["700"],
+          info: colors.cyan["200"],
+          success: colors.green["200"],
+          warning: colors.orange["200"],
+          error: colors.red["300"],
+        },
+      },
     ],
+    darkTheme: "ddrfinder-dark",
   },
   plugins: [daisyui],
 };
