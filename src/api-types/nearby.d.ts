59970d397ebe8e319b2f079dccd39b6a2c302302
Andrew67 <andres@andrew67.com>
2024-05-10 00:43:32 -0400

    Set up placeholders, loading state, first pass of a loaded list, by hard-coding the API and map URLs and not wiring up error states yet

---
+++ src/api-types/nearby.d.ts
@@ -0,0 +1,30 @@
+import type { ArcadeLocation, ArcadeLocationApiResponse } from "./all.d.ts";
+
+/**
+ * TL;DR the nearby API response is the same as the all API, but with a distance field
+ */
+
+export interface ArcadeLocationWithDistance extends ArcadeLocation {
+  properties: ArcadeLocation["properties"] & {
+    /**
+     * Approximate distance to this location from the coordinates provided to the search query, in kilometers.
+     * Maximum precision of 2 decimal digits.
+     */
+    distanceKm: number;
+  };
+}
+
+export interface NearbyApiResponse
+  extends ArcadeLocationApiResponse<ArcadeLocationWithDistance> {
+  /**
+   * "Bounding Box" concept in GeoJSON.
+   * See {@link https://www.rfc-editor.org/rfc/rfc7946#section-5}.
+   * Provides information back to the frontend about the bounding box used in the search.
+   */
+  bbox: [
+    southWestLongitude: number,
+    southWestLatitude: number,
+    northEastLongitude: number,
+    northEastLatitude: number,
+  ];
+}
--- src/components/nearby/NearbyPage.tsx
+++ src/components/nearby/NearbyPage.tsx
@@ -1,12 +1,120 @@
 import { IconCurrentLocation } from "@tabler/icons-preact";
 import type { h, FunctionComponent } from "preact";
+import { useEffect, useMemo, useState } from "preact/hooks";
+
+import type { NearbyApiResponse } from "../../api-types/nearby";
 
 export const NearbyPage: FunctionComponent = () => {
+  const [apiResponse, setApiResponse] = useState<NearbyApiResponse>({
+    type: "FeatureCollection",
+    bbox: [0, 0, 0, 0],
+    features: [],
+  });
+
+  const [mapUrl, setMapUrl] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const mapPlaceholder = useMemo(
+    () => (
+      <div
+        className={`h-52 bg-base-200 max-w-screen-sm sm:mx-4 sm:rounded-box ${
+          isLoading ? "skeleton rounded-none" : ""
+        }`}
+        aria-label="Empty placeholder for map image"
+      ></div>
+    ),
+    [isLoading],
+  );
+
+  const mapImage = useMemo(
+    () => (
+      <img
+        className={`h-52 bg-base-200 max-w-screen-sm sm:mx-4 sm:rounded-box`}
+        src={mapUrl}
+        alt="Map of your location and nearby arcade locations"
+      ></img>
+    ),
+    [mapUrl],
+  );
+
+  const arcadeListPlaceholder = useMemo(
+    () =>
+      new Array(5).fill(0).map((_, i) => (
+        <li
+          key={`placeholder-${i}`}
+          class={`collapse bg-base-200 ${isLoading ? "skeleton" : ""}`}
+          aria-label="Empty placeholder for arcade location"
+        >
+          <div class="collapse-title"></div>
+        </li>
+      )),
+    [isLoading],
+  );
+
+  const arcades = apiResponse.features;
+  const arcadeList = useMemo(
+    () =>
+      arcades.map((loc) => {
+        const hasDanceGames =
+          loc.properties["has:ddr"] > 0 ||
+          loc.properties["has:piu"] > 0 ||
+          loc.properties["has:smx"] > 0;
+        return (
+          <li key={`location-${loc.id}`}>
+            <article class="collapse collapse-arrow bg-base-200 print:collapse-open print:border print:rounded">
+              <input type="checkbox" name={`arcade-accordion-${loc.id}`} />
+              <div className="collapse-title text-lg font-medium">
+                {loc.properties.name}
+              </div>
+              <ul className="collapse-content">
+                {hasDanceGames && (
+                  <li class="flex gap-1 items-baseline">
+                    Games:
+                    {loc.properties["has:ddr"] > 0 && (
+                      <span class="badge badge-primary">DDR</span>
+                    )}
+                    {loc.properties["has:piu"] > 0 && (
+                      <span class="badge badge-secondary">PIU</span>
+                    )}
+                    {loc.properties["has:smx"] > 0 && (
+                      <span class="badge badge-accent">SMX</span>
+                    )}
+                  </li>
+                )}
+              </ul>
+            </article>
+          </li>
+        );
+      }),
+    [arcades],
+  );
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      // TODO: URL builder (source, lat/lng, game filters)
+      // "https://ddrfinder-api.andrew67.com/v4/nearby/ziv.geojson?ll=35.6894,139.6917",
+      "https://ddrfinder-api.andrew67.com/v4/nearby/ziv.geojson?ll=32.9659,-97.0453",
+    )
+      .then((response) => response.json())
+      .then((apiResponse) => {
+        // TODO: API success but no arcades found state
+        setApiResponse(apiResponse);
+        setMapUrl(
+          // TODO: URL builder (user location, arcade locations)
+          "https://api.mapbox.com/styles/v1/andrew67/clrwbi529011u01qseesn4gj9/static/pin-s+b91c1c(-97.0453,32.9659),pin-l-1+a21caf(-97.0444,32.9638),pin-l-2+a21caf(-97.0599,32.9413),pin-l-3+a21caf(-97.0816,32.9201),pin-l-4+a21caf(-97.1576,32.9417),pin-l-5+a21caf(-97.1032,32.8736)/auto/320x208@2x?access_token=pk.eyJ1IjoiYW5kcmV3NjciLCJhIjoiY2lxMDlvOHZoMDAxOWZxbm9tdnR1NjVubSJ9.35GV_5ZM6zS2R5KQCwBWqw",
+        );
+      })
+      // TODO: API error state
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <>
       <h2 class="text-2xl mt-4 mx-4">Your location:</h2>
       {/* Add border border-primary when result is in */}
-      <div class="h-52 bg-base-200"></div>
+      {mapUrl.length === 0 && mapPlaceholder}
+      {mapUrl.length > 0 && mapImage}
       <div class="mx-4 mb-4">
         <p class="h-6">Accuracy: approximately 80 meters</p>
         <p class="mb-4">
@@ -17,15 +125,9 @@ export const NearbyPage: FunctionComponent = () => {
 
         <h2 class="text-2xl">Nearby arcades:</h2>
         <p class="h-6 mb-2">&copy; OpenStreetMap Contributors</p>
-        <ul>
-          {new Array(5).fill(0).map((_, i) => (
-            // Add collapse-plus when result is in
-            <li class="collapse bg-base-200 mb-2 print:collapse-open print:border print:rounded">
-              <input type="checkbox" name={`arcade-accordion-${i}`} />
-              <div class="collapse-title text-lg sm:text-xl font-medium"></div>
-              {/*<div class="collapse-content"></div>*/}
-            </li>
-          ))}
+        <ul class="grid gap-2 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+          {arcades.length === 0 && arcadeListPlaceholder}
+          {arcades.length > 0 && arcadeList}
         </ul>
 
         <footer class="mt-6">
