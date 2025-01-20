36afe71207f5467d1b04fd1df4cda97e0f82f72b
Andrew67 <andres@andrew67.com>
2025-01-19 23:33:27 -0500

    Use `maps:` links on Apple devices and `geo:` links on Android for the Navigate button.

--- src/components/nearby/useArcadeListItemLinks.ts
+++ src/components/nearby/useArcadeListItemLinks.ts
@@ -9,6 +9,16 @@ type ArcadeListItemLinks = {
   moreInfoMobileUrl: string;
 };
 
+// TODO: Preferred navigation app setting
+let getNavigateUrl = (lat: number, lng: number, name: string) =>
+  `https://maps.google.com/?q=loc:${lat},${lng}(${encodeURIComponent(name)})`;
+if (/Mac OS X/.test(navigator.userAgent))
+  getNavigateUrl = (lat, lng, name) =>
+    `maps:?q=${encodeURIComponent(name)}&ll=${lat},${lng}`;
+else if (/Android/.test(navigator.userAgent))
+  getNavigateUrl = (lat, lng, name) =>
+    `geo:${lat},${lng}?q=${lat},${lng}(${encodeURIComponent(name)})`;
+
 /** Prepares the "Navigate" and "More Info" URLs for a given location */
 export const useArcadeListItemLinks = (
   location: ArcadeLocation,
@@ -21,9 +31,7 @@ export const useArcadeListItemLinks = (
   const { name, sid } = location.properties;
 
   return {
-    navigateUrl: `https://maps.google.com/?q=loc:${lat},${lng}(${encodeURIComponent(
-      name,
-    )})`,
+    navigateUrl: getNavigateUrl(lat, lng, name),
     moreInfoUrl: activeSource["url:info"].replace("${sid}", sid),
     moreInfoMobileUrl: activeSource["url:info:mobile"].replace("${sid}", sid),
   };
