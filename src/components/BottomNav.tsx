f85e05342259b02ee8d501ba74be0e85adbb645e
Andrew67 <andres@andrew67.com>
2025-03-08 02:31:38 -0500

    Enable new `/app/nearby` as main Locate button in index page! TODO: /locator compatibility redirect in order to retire it fully. Existing links continue to go to it. Temporary bottom nav with links to `/ng` for Explore and 3rd button as Calculator instead of Menu.

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,6 +1,10 @@
 import type { h, FunctionComponent } from "preact";
-import { useCallback } from "preact/hooks";
-import { IconLocationSearch, IconMap2, IconMenu2 } from "@tabler/icons-preact";
+import { useCallback, useEffect, useState } from "preact/hooks";
+import {
+  IconCalculator,
+  IconLocationSearch,
+  IconMap2,
+} from "@tabler/icons-preact";
 import { $router } from "../stores/router.ts";
 import { useStore } from "@nanostores/preact";
 import { getPagePath } from "@nanostores/router";
@@ -8,12 +12,23 @@ import { getPagePath } from "@nanostores/router";
 export const BottomNav: FunctionComponent<{
   initialPage: string | undefined;
 }> = ({ initialPage = "" }) => {
-  const pageRoute = useStore($router)?.route ?? initialPage;
+  const page = useStore($router);
+  const pageRoute = page?.route ?? initialPage;
 
   const activeLinkClasses = useCallback(
     (page: string) => (page === pageRoute ? "active bg-base-200" : ""),
     [pageRoute],
   );
+
+  // Future: getPagePath($router, "explore", {}, page?.search)
+  // Today: compatibility open of existing /ng UI
+  const [ngLink, setNgLink] = useState("/ng");
+  useEffect(() => {
+    if (!page?.search) return setNgLink("/ng");
+    const ngParams = new URLSearchParams(page.search);
+    ngParams.set("z", "14");
+    return setNgLink(`/ng?${ngParams}`);
+  }, [page?.search]);
 
   return (
     <>
@@ -25,19 +40,24 @@ export const BottomNav: FunctionComponent<{
           <IconLocationSearch aria-hidden="true" />
           <span className="btm-nav-label">Nearby</span>
         </a>
-        <a
-          href={getPagePath($router, "explore")}
-          className={activeLinkClasses("explore") + " short:flex-row"}
-        >
+        <a href={ngLink} target="_self" className="short:flex-row">
           <IconMap2 aria-hidden="true" />
           <span className="btm-nav-label">Explore</span>
         </a>
-        <a
+        {/*        <a
           href={getPagePath($router, "menu")}
           className={activeLinkClasses("menu") + " short:flex-row"}
         >
           <IconMenu2 aria-hidden="true" />
           <span className="btm-nav-label">Menu</span>
+        </a>*/}
+        <a
+          href="https://ddrcalc.andrew67.com/"
+          target="_blank"
+          className="short:flex-row"
+        >
+          <IconCalculator aria-hidden="true" />
+          <span className="btm-nav-label">Calculator</span>
         </a>
       </nav>
       {/* This hack won't be needed in DaisyUI 5.x */}
