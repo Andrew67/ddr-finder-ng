86d02ada6a266b9840568c3f7fe3378c752e660d
Andrew67 <andres@andrew67.com>
2025-04-05 14:16:17 -0400

    Load URL changes into `$mapLocation`.

--- src/stores/explore/mapLocation.ts
+++ src/stores/explore/mapLocation.ts
@@ -11,6 +11,9 @@ export type MapLocation = {
   zoom: number;
 };
 
+/** TODO: Dynamic digits based on zoom level */
+const LAT_LNG_DIGITS = 5;
+
 /**
  * On the "explore" page, this feeds into the URL and gets used for moving the map.
  * Saves the last location to local storage to be used on next app open.
@@ -31,7 +34,6 @@ export const $mapLocation = persistentAtom<MapLocation>(
 
 /**
  * Flow map movement events back into our state and the URL.
- * TODO: Trim lat/lng digits based on zoom level
  * TODO: Centralize URL updating code, such that ll/z can be set on page load. Today, if we did that, they'd override source/filters from local storage
  */
 export function setLocationFromMap(newLocation: MapLocation) {
@@ -39,8 +41,8 @@ export function setLocationFromMap(newLocation: MapLocation) {
 
   const page = $router.get();
   if (page?.route === "explore") {
-    const latitudeFixed = newLocation.center.lat.toFixed(5);
-    const longitudeFixed = newLocation.center.lng.toFixed(5);
+    const latitudeFixed = newLocation.center.lat.toFixed(LAT_LNG_DIGITS);
+    const longitudeFixed = newLocation.center.lng.toFixed(LAT_LNG_DIGITS);
     redirectPage($router, page.route, page.params, {
       ...page.search,
       ll: `${latitudeFixed},${longitudeFixed}`,
@@ -48,4 +50,48 @@ export function setLocationFromMap(newLocation: MapLocation) {
     });
   }
 }
-// TODO: Move map when URL changes
+
+// Moves the map on the explore page using the URL parameters when changed
+$router.subscribe((page) => {
+  // On the "nearby" page, the parameters feed into `$userLocation` instead
+  if (page?.route !== "explore") return;
+
+  const ll = page?.search["ll"];
+  const z = page?.search["z"];
+  if (!ll || !z) return;
+
+  // Input validation: both latitude and longitude provided
+  let [latitude, longitude] = ll.split(",");
+  if (!latitude || !longitude) return;
+
+  // Input validation: numeric inputs provided
+  const latitudeNumeric = Number(latitude);
+  const longitudeNumeric = Number(longitude);
+  let zoomNumeric = Number(z);
+  if (isNaN(latitudeNumeric) || isNaN(longitudeNumeric) || isNaN(zoomNumeric))
+    return;
+
+  // Input validation: latitude between +/- 90 and longitude between +/- 180
+  if (latitudeNumeric < -90 || latitudeNumeric > 90) return;
+  if (longitudeNumeric < -180 || longitudeNumeric > 180) return;
+
+  // Compatibility: clamp zoom to between 1 and 22
+  if (zoomNumeric < 1) zoomNumeric = 1;
+  if (zoomNumeric > 22) zoomNumeric = 22;
+
+  // If this is already our current location, don't set it
+  const currentLocation = $mapLocation.get();
+  if (
+    latitudeNumeric.toFixed(LAT_LNG_DIGITS) ===
+      currentLocation.center.lat.toFixed(LAT_LNG_DIGITS) &&
+    longitudeNumeric.toFixed(LAT_LNG_DIGITS) ===
+      currentLocation.center.lng.toFixed(LAT_LNG_DIGITS) &&
+    zoomNumeric.toFixed(1) === currentLocation.zoom.toFixed(1)
+  )
+    return;
+
+  $mapLocation.set({
+    center: { lat: latitudeNumeric, lng: longitudeNumeric },
+    zoom: zoomNumeric,
+  });
+});
