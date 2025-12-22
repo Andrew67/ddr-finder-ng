f5abfa7b453c1d68ab51034759c5dbe28dbfa125
Andrew67 <andres@andrew67.com>
2025-12-21 20:43:28 -0500

    Initial upgrade of TailwindCSS to v4 and DaisyUI to v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [preact()],
   build: {
     format: "preserve",
   },
   vite: {
+    plugins: [tailwindcss()],
     server: {
       allowedHosts: true,
     },
